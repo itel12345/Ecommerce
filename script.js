d31ae1d98158a64bcc35edc9ec0a90354d56a0d0
Loop Games <itelemma0@gmail.com>
2025-05-18 00:17:55 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ function openModal(imgElement) {
 function closeModal() {
   document.getElementById("imageModal").style.display = "none";
 }
+
+  const topBtn = document.getElementById("topBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      topBtn.style.display = "block";
+    } else {
+      topBtn.style.display = "none";
+    }
+  };
